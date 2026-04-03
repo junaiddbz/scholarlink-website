fabe232e9263e7cef4437134bb57497825529319
Junaid Ahmed <junaiddbz01@gmail.com>
2026-04-03 06:21:53 +0500

    Fix: Add dynamic export to all pages with useSearchParams components

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
 import { BackToTop } from "@/components/back-to-top";
--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useMemo, useState } from "react";
 import Link from "next/link";
 import { Header } from "@/components/header";
